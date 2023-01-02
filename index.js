1344d457585e10c655dc4b397b5ccf639a4fa475
Barnaan2 <barnaantekalign@gmail.com>
2023-01-02 13:06:36 -0800

    default user location is used to set view

--- index.js
+++ index.js
@@ -2,23 +2,82 @@ window.onload = ()=>{
     if(navigator.geolocation){  navigator.geolocation.getCurrentPosition(search);}
   };
 
-let  map = L.map('map').setView([3.5399846,39.0528407], 13);
 
-googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+  function search(value){
+
+
+    let  map = L.map('map').setView([value.coords.latitude,value.coords.longitude], 13);
+
+ L.tileLayer('http://{s}.google.com/vt/lyrs=s,p&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3'],
     attribution: '&copy; <a href="https://www.github.com/barnaan2">Bernabas Tekkalign</a>'
 }).addTo(map);
-  function search(value){
+
     alert(` latitude point is  ${value.coords.latitude} and longitude point is ${value.coords.longitude} `)
-     var marker = L.marker([value.coords.latitude, value.coords.longitude]).addTo(map);
-     var hotel = L.marker([3.5399846,39.0528407]).addTo(map);
-     hotel.bindPopup("<b>Central hotel is here </b><br>")
-  marker.bindPopup("<b>You are here </b><br>").openPopup();
+    let yourPosition = L.marker([value.coords.latitude, value.coords.longitude]).addTo(map);
+       yourPosition.bindPopup("<b>You are here </b><br>").openPopup();
+
+      
   }
+
+  var popup = L.popup();
+
+  function onMapClick(e) {
+      popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(map);
+  }
+  
+  map.on('click', onMapClick);
 
 
   //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //       maxZoom: 19,
 //       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 //   }).addTo(map);
+
+/*
+DIFFERENT TYPE OF GOOGLE MAP INCASE YOU WANT TO USE
+
+GOOGLE TERRIAN:
+googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+Note the difference in the "lyrs" parameter in the URL:
+Hybrid: s,h;
+Satellite: s;
+Streets: m;
+Terrain: p;
+
+GOOGLE SAT:
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+GOOGLE HYBRID:
+
+googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+
+
+
+   GOOGLE STREET: 
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+
+
+*/
